ba1f54159c32aa55206b8dc2791238a6ad8952ce
Zheng Hanbin <hanbinzheng54@gmail.com>
2025-09-06 17:01:19 +0800

    add the ga for question 2

---
+++ question2_ga.py
@@ -0,0 +1,163 @@
+import numpy as np
+from typing import List, Tuple
+from GAFrame import SingleDronePlan, Evaluator
+from GAFrame import Genome, Individual, GeneticAlgorithm
+
+
+class Genome2(Genome):
+    def __init__(self,
+                 drone_plans: List[SingleDronePlan]):
+        super().__init__(drone_plans)
+
+    def get_info(self) -> str:
+        # get all the info
+        drone_plan = self.drone_plans[0]
+        vel_dir = drone_plan.vel_dir
+        vel_val = drone_plan.vel_val
+        t_release = drone_plan.t_release
+        t_wait = drone_plan.t_wait
+
+        vel_info = f"vel_dir: {vel_dir}, vel_val: {vel_val}, "
+        t_info = f"release at: {t_release}s, wait {t_wait}s before explode"
+
+        return vel_info + t_info
+
+    def mutate(self, mutation_rate: float):
+        # get the plan
+        plan = self.drone_plans[0]
+
+        # mute in the vel_dir
+        if np.random.rand() < mutation_rate:
+            new_vel_dir = plan.vel_dir + np.random.uniform(-np.pi/18, np.pi/18)
+            plan.vel_dir = new_vel_dir % (2 * np.pi)  # check the boundary
+
+        # mute the vel_val
+        if np.random.rand() < mutation_rate:
+            new_vel_val = plan.vel_val + np.random.uniform(-5, 5)
+            plan.vel_val = np.clip(new_vel_val, 70.0, 140.0)  # check boundary
+
+        # mute t_release
+        if np.random.rand() < mutation_rate:
+            new_t_release = plan.t_release[0] + np.random.uniform(-0.2, 0.2)
+            plan.t_release = [max(0, new_t_release)]
+
+        # mute t_wait
+        if np.random.rand() < mutation_rate:
+            new_t_wait = plan.t_wait[0] + np.random.uniform(-0.2, 0.2)
+            plan.t_wait = [max(0, new_t_wait)]
+
+    def crossover(self, pair_genome: "Genome2") -> Tuple["Genome2", "Genome2"]:
+        # do the crossover here
+        copy1, copy2 = self.get_copy(), pair_genome.get_copy()
+        plan1 = copy1.drone_plans[0]
+        plan2 = copy2.drone_plans[0]
+
+        # get the copy
+        parent1 = np.array([
+            plan1.vel_dir,
+            plan1.vel_val,
+            plan1.t_release[0],
+            plan1.t_wait[0]
+        ])
+        parent2 = np.array([
+            plan2.vel_dir,
+            plan2.vel_val,
+            plan2.t_release[0],
+            plan2.t_wait[0]
+        ])
+
+        magnitude = 0.5
+
+        child1 = magnitude * parent1 + (1 - magnitude) * parent2
+        child2 = magnitude * parent2 + (1 - magnitude) * parent1
+
+        plan1.vel_dir, plan2.vel_dir = child1[0], child2[0]
+        plan1.vel_val, plan2.vel_val = child1[1], child2[1]
+        plan1.t_release, plan2.t_release = [child1[2]], [child2[2]]
+        plan1.t_wait, plan2.t_wait = [child1[3]], [child2[3]]
+
+        return copy1, copy2
+
+class Evaluator2(Evaluator):
+    def __init__(self):
+        missile_posi = np.array([20000.0, 0.0, 2000.0])
+        super().__init__([missile_posi])
+
+    def evaluate(self, genome: Genome2) -> float:
+        plan = genome.drone_plans[0]
+        t_explode = plan.t_release[0] + plan.t_wait[0]
+        missile_posi = (
+            self.missile_init_posi[0] + self.v_missile[0] * t_explode
+        )
+
+        time_list = []
+        for t in np.linspace(0.0, 20.0, 2001):
+            smoke_posi_now = plan.get_smoke_posi(t + t_explode)[0]
+            if smoke_posi_now is None:
+                continue
+
+            missile_posi_now = missile_posi + t * self.v_missile[0]
+            if self.whether_blocked(missile_posi_now, smoke_posi_now):
+                time_list.append(t)
+
+        if len(time_list) < 2:
+            return 0.0
+        else:
+            return time_list[-1] - time_list[1]
+
+
+class GeneticAlgorithm2(GeneticAlgorithm):
+    def __init__(
+            self,
+            pop_size: int,
+            evaluator: Evaluator2,
+            crossover_rate: float,
+            mutation_rate: float,
+            num_gen: int
+    ):
+        super().__init__(
+            pop_size = pop_size,
+            evaluator = evaluator,
+            crossover_rate = crossover_rate,
+            mutation_rate = mutation_rate,
+            num_gen = num_gen
+        )
+
+    def initialize_pop(self, pop_size: int) -> List[Individual]:
+        pop = []
+
+        # set an valid position
+        plan = SingleDronePlan(
+            init_posi = np.array([17800.0, 0.0, 1800.0]),
+            vel_dir = - np.pi,
+            vel_val = 120.0,
+            t_release = [1.5],
+            t_wait = [3.6]
+        )
+        genome = Genome2([plan])
+        pop.append(Individual(genome))
+
+        # random initialize
+        for i in range(pop_size - 1):
+            plan = SingleDronePlan(
+                init_posi = np.array([17800.0, 0.0, 1800.0]),
+                vel_dir = np.random.rand() * 2 * np.pi,
+                vel_val = np.random.uniform(70, 140),
+                t_release = [np.random.rand() * 5],
+                t_wait = [np.random.rand() * 5]
+            )
+            genome = Genome2([plan])
+            pop.append(Individual(genome))
+
+        return pop
+
+
+if __name__ == '__main__':
+    GA = GeneticAlgorithm2(
+        pop_size = 50,
+        evaluator = Evaluator2(),
+        crossover_rate = 0.5,
+        mutation_rate = 0.5,
+        num_gen = 50
+    )
+    GA.evolve()
